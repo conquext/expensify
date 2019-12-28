bf073934707db1f048d79950daee714115a35913
Farce <intbusfor@gmail.com>
2019-12-29 00:31:05 +0100

    Build for heroku

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const app = express();
 console.log("dirname", __dirname);
 console.log(path.join(__dirname, "..", "dist"));
 const publicPath = path.join(__dirname, "..", "dist");
+console.log(path.join(publicPath, "/index.html"));
+
 const { PORT = 30001 } = process.env;
 
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.join(publicPath, "/index.html"));
 });
 
 app.listen(PORT, () => {

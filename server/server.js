aaa77f960cb11da0505a84ab38cc549f944be9d5
Farce <intbusfor@gmail.com>
2019-12-29 00:38:28 +0100

    Build for heroku

--- server/server.js
+++ server/server.js
@@ -3,9 +3,18 @@ const express = require("express");
 
 const app = express();
 console.log("dirname", __dirname);
-console.log(path.join(__dirname, "..", "dist"));
+console.log(path.join(__dirname, "...", "dist"));
 const publicPath = path.join(__dirname, "..", "dist");
 console.log(path.join(publicPath, "/index.html"));
+console.log("dir", path.dirname(require.main.filename));
+
+const fs = require("fs");
+
+fs.readdir(publicPath, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});
 
 const { PORT = 30001 } = process.env;
 
